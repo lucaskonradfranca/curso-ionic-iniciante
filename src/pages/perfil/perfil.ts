0c308255be4372954c2dab6c70885af2717ff306
lucaskonradfranca <lucaskonrad92@gmail.com>
2018-02-25 11:08:56 -0300

    APP Finalizado

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions} from '@ionic-native/camera';
 
 /**
  * Generated class for the PerfilPage page.
@@ -12,14 +13,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-perfil',
   templateUrl: 'perfil.html',
+  providers: [
+    Camera
+  ]
 })
 export class PerfilPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  img = '';
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              private camera: Camera) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PerfilPage');
+  }
+
+  tirarFoto(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData)=>{
+      this.img = 'data:image/jpeg;base64,'+imageData;
+    }, err =>{
+      console.log(err);
+    });
   }
 
 }
